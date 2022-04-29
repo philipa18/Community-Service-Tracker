59951378bbe28d422e64a8dee84f5a1779d84626
NathanielBenyam <nbenyam@montgomerycollege.edu>
2022-04-29 13:54:22 -0400

    Nathaniel layed out a foundation for all group members to start working. The purpose of these edits are for each group member to have access to each others' work. I have added an initial activity file with a corresponding layout file for each workflow we have designed.

--- app/src/main/java/com/example/communityservicetracker/MainActivity.kt
+++ app/src/main/java/com/example/communityservicetracker/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.example.communityservicetracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    /*
+    Temporary buttons that lead to separate workflows. Plan to get rid of
+    these once we all have our workflows ready.
+    */
+    private lateinit var nathanielButton: Button
+    private lateinit var inesButton: Button
+    private lateinit var philipButton: Button;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        /*
+        Set up buttons to lead to each group members' workflows.
+        Should help with collaboration
+         */
+        philipButton = findViewById(R.id.PhilipButton)
+        inesButton = findViewById(R.id.InesButton)
+        nathanielButton = findViewById(R.id.NathanielButton)
+
+        // Set onClickListener for each button
+        philipButton.setOnClickListener(){
+            processClick(it)
+        }
+
+        inesButton.setOnClickListener(){
+            processClick(it)
+        }
+
+        nathanielButton.setOnClickListener(){
+            processClick(it)
+        }
+
+    }
+
+    // Depending on which button is clicked, we will be taken to that page
+    fun processClick(v: View) {
+
+        // First argument: current class
+        // Second argument: the class that we will be directed to depending
+        //      on the id of the button that we clicked
+
+        var activityIntent = Intent(this@MainActivity,
+
+            when ((v as Button).text)  {
+
+                "Sign-in and account creation workflow" ->
+                    SignInAccCreationActivity::class.java
+                "Student Workflow" ->
+                    StudentActivity::class.java
+                else -> OrgActivity::class.java
+            }
+
+        )
+
+        // Go to that class by displaying its contents on the screen
+        startActivity(activityIntent)
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/communityservicetracker/SignInAccCreationActivity.kt
@@ -0,0 +1,20 @@
+package com.example.communityservicetracker
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+// Philip
+class SignInAccCreationActivity : Activity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_account_creation)
+    }
+
+
+
+}
