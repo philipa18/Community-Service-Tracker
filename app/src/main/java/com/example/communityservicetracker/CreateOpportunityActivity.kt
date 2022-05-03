93a5d3a39235a297d8795927108e617298ac1a9e
NathanielBenyam <nbenyam@montgomerycollege.edu>
2022-05-03 14:53:17 -0400

    CreateOpportunityActivity for orgs to create listings. Also, updated opportunity_item.xml

---
+++ app/src/main/java/com/example/communityservicetracker/CreateOpportunityActivity.kt
@@ -0,0 +1,23 @@
+package com.example.communityservicetracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+// Page that allows organization to create a new opportunity to be posted
+class CreateOpportunityActivity :AppCompatActivity() {
+
+    // Typical three buttons at bottom of page
+
+    // Two buttons (submit or cancel) above the three
+
+    // Provide a form for user to fill out
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
