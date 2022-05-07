98c2c3269572c551d81634be1c0d434dde49cb48
Philip Alexander <philipalexander39@gmail.com>
2022-05-07 16:44:16 -0400

    Creation of OpportunityDetailActivity.kt

---
+++ app/src/main/java/com/example/communityservicetracker/OpportunityDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.communityservicetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpportunityDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opportunity_detail)
+    }
+}
