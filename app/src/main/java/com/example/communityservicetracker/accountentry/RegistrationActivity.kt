2d570a44fc8945bb3f3473286de39535358affd4
Philip Alexander <philipalexander39@gmail.com>
2022-05-08 21:35:19 -0400

    Added validation and mandatory inputs for student fields.

--- app/src/main/java/com/example/communityservicetracker/accountentry/RegistrationActivity.kt
+++ app/src/main/java/com/example/communityservicetracker/accountentry/RegistrationActivity.kt
@@ -41,8 +41,6 @@ class RegistrationActivity : AppCompatActivity() {
         val accountType = intent.getIntExtra("account_type",0)
         if (accountType == 0)
             header.text = "Register a new student account"
-//            studentGrade.visibility = View.VISIBLE
-//            studentGoal.visibility = View.VISIBLE
         else {
             header.text = "Register a new organization account"
             studentGrade.visibility = View.GONE
@@ -57,15 +55,45 @@ class RegistrationActivity : AppCompatActivity() {
         val email: String = userEmail.text.toString()
         val password: String = userPassword.text.toString()
         val name: String = userName.text.toString()
+        val grade: String = studentGrade.text.toString()
+        val goal: String = studentGoal.text.toString()
 
-//        if (!validator.validEmail(email)) {
-//            Toast.makeText(applicationContext, "Please enter valid email...", Toast.LENGTH_LONG).show()
-//            return
-//        }
-//        if (!validator.validPassword(password)) {
-//            Toast.makeText(applicationContext, "Password must contain 8 characters with one letter and one number!", Toast.LENGTH_LONG).show()
-//            return
-//        }
+        val emailRegex = Regex("(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'" +
+                "*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x" +
+                "5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z" +
+                "0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4" +
+                "][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z" +
+                "0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|" +
+                "\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])")
+
+        if (email.isEmpty() || !emailRegex.matches(email)) {
+            Toast.makeText(applicationContext, "Please enter valid email", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val passwordRegex = Regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}\$")
+
+        if (password.isEmpty() || !passwordRegex.matches(password)) {
+            Toast.makeText(applicationContext, "Password must contain 8 characters with one letter and one number", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (name.isEmpty()) {
+            Toast.makeText(applicationContext, "Please enter a name", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (accountType == 0 && grade.isEmpty()) {
+            Toast.makeText(applicationContext, "Please enter your grade", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (accountType == 0 && goal.isEmpty()) {
+            Toast.makeText(applicationContext, "Please enter your hour goal", Toast.LENGTH_LONG).show()
+            return
+        }
+
+
 
         val x = mAuth!!.createUserWithEmailAndPassword(email, password)
 
@@ -76,7 +104,6 @@ class RegistrationActivity : AppCompatActivity() {
                 mDB.child("users").child(uid).child("account_type").setValue(accountType)
                 mDB.child("users").child(uid).child("name").setValue(name)
 
-                val accountType = intent.getIntExtra("account_type",0)
                 if (accountType == 0) {
                     mDB.child("users")
                         .child(uid)
@@ -85,11 +112,11 @@ class RegistrationActivity : AppCompatActivity() {
                     mDB.child("users")
                         .child(uid)
                         .child("grade")
-                        .setValue(studentGrade.text.toString())
+                        .setValue(grade)
                     mDB.child("users")
                         .child(uid)
                         .child("hour_goal")
-                        .setValue(studentGoal.text.toString())
+                        .setValue(goal)
                 }
 
                 Toast.makeText(applicationContext, "Registration successful!", Toast.LENGTH_LONG).show()
